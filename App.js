e6508d9cb907e94df574217cafb06dd9e164bc02
kimhouyoung <sdh230302@sdh.hs.kr>
2023-12-27 22:47:53 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,49 +1,45 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function App(){
-  const [Todo,setTodo] = useState([]);
-  const [Task,setTask] = useState('');
+function App() {
+  const [Todo, setTodos] = useState([]);
+  const [Task, setTask] = useState('');
 
-  const Arrlist = () =>{
+  const addTodo = () => {
     if (Task.trim() !== '') {
-      setTodo([...Todo,Task]);
-      setTask('')
-    }
-  }
-
-  const deleteTodo=(index) =>{
-  const newTodo=[...Todo];
-  newTodo.splice(index, 1);
-  setTodo(newTodo);
-  }
-   const handleKeyDown = (e) => {
-      if (e.key === 'Enter') {
-      Arrlist();
+      setTodos([...Todo, Task]);
+      setTask('');
     }
   };
-  return(
+
+  const deleteTodo = (index) => {
+    const newTodos = [...Todo];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
     <div className="App">
       <h1>Todo</h1>
       <input
-      type="text"
-      value={Task}
-      onChange={(e) => setTask(e.target.value)}
-      onKeyDown={handleKeyDown}
+        type="text"
+        value={Task}
+        onChange={(e) => setTask(e.target.value)}
       />
-      <button onClick={Arrlist}>
-      검색
+      <button onClick={addTodo}>
+        검색
       </button>
       <ul>
-        {Todo.map((todo, index) => (
+        {Todo.map((Todo, index) => (
           <li key={index}>
-            {todo}
-            <button onClick={()=>deleteTodo(index)}>삭제</button>
+            {Todo}
+            <button onClick={() => deleteTodo(index)}>
+              삭제
+            </button>
           </li>
         ))}
       </ul>
     </div>
-  )
-};
-//자퇴 신청서
-//자살하는법 
+  );
+}
+
 export default App;
